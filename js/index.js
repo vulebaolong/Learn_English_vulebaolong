89afefa79b222869cc90cce30489c9a135f09454
vulebaolong <vulebaolong@gmail.com>
2023-04-08 21:33:46 +0700

    done vowels Chart double

--- js/index.js
+++ js/index.js
@@ -4,9 +4,10 @@ const $$ = (selector) => document.querySelectorAll(selector);
 const phonemeChartEl = $(".phonemeChart");
 phonemeChartEl.addEventListener("click", (e) => {
     const name = e.target.classList[0];
+    if (!name) return;
     const arrName = name.split("_");
 
-    if (arrName[1] === "vowelsSingle") {
+    if (arrName[1] === "vowelsSingle" || arrName[1] === "vowelsDouble") {
         const audio = $(`.audio_${arrName[1]}_${arrName[2]}`);
         audio.play();
     }
