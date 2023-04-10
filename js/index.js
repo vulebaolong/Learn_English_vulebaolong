0b244a13c14ecf74c7796eac565c97351a5b49b9
vulebaolong <vulebaolong@gmail.com>
2023-04-10 22:39:36 +0700

    test audio 2

--- js/index.js
+++ js/index.js
@@ -14,17 +14,39 @@ phonemeChartEl.addEventListener("click", (e) => {
 });
 
 const vocabularyListEl = $(".vocabulary_list");
-vocabularyListEl.addEventListener("click", (e) => {
+vocabularyListEl.addEventListener("click", async (e) => {
     const name = e.target.classList[0];
 
     // console.log(name);
+    // if (name === "vocabulary_item") {
+    //     const value = e.target.innerText;
+    //     fetch(
+    //         `https://translate.google.com/translate_tts?ie=UTF-&&client=tw-ob&tl=en&q=${value}`
+    //     );
+    //     console.log(e.target);
+    //     console.dir(e.target);
+    //     console.log(e.target.innerText);
+    // }
+
     if (name === "vocabulary_item") {
         const value = e.target.innerText;
-        fetch(
-            `https://translate.google.com/translate_tts?ie=UTF-&&client=tw-ob&tl=en&q=${value}`
-        );
-        console.log(e.target);
-        console.dir(e.target);
-        console.log(e.target.innerText);
+        // const msg = new SpeechSynthesisUtterance(`${value}`);
+        // window.speechSynthesis.speak(msg);
+        say(`${value}`);
     }
 });
+
+function say(m) {
+    const msg = new SpeechSynthesisUtterance();
+    const voices = window.speechSynthesis.getVoices();
+    // msg.voice = voices[5];
+    msg.voice = voices[6];
+    msg.voiceURI = "native";
+    msg.volume = 1;
+    msg.rate = 1;
+    msg.pitch = 1;
+    msg.text = m;
+    msg.lang = "en-UK";
+    speechSynthesis.speak(msg);
+}
+say("zebra");
